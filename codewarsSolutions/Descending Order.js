39fe075139f8e63469dad9049a1cb8b7fb0bda3b
Eduardo <emendoza3843@ppsc.edu.mx>
2021-03-03 17:50:53 -0600

    agragar codigos codewars

---
+++ codewarsSolutions/Descending Order.js
@@ -0,0 +1,11 @@
+function descendingOrder(n){
+    //...
+  
+    return parseInt(n.toString().split("").sort((a,b)=> b-a).join(""));
+    //convertir a string 
+    //separar numero por numero 
+    // ordenar con sort 
+    //volver a unir 
+}
+  
+//https://www.codewars.com/kata/5467e4d82edf8bbf40000155
---
+++ codewarsSolutions/Reversed Strings.js
@@ -0,0 +1,5 @@
+function solution(str){
+    return str.split("").reverse().join("")
+    
+}
+  //https://www.codewars.com/kata/5168bb5dfe9a00b126000018
